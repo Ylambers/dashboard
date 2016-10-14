12569d3cf2237f6956d8ce4136c83d96790bb41a
Yaron Lambers <y.lambers@outlook.com>
2016-10-14 17:36:52 +0200

    change git ignore

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ylamb
+ * Date: 14-10-2016
+ * Time: 17:30
+ */
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
