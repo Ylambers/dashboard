3c1df4ddca6c4518c63b52839bc55263f653e57d
Yaron Lambers <y.lambers@outlook.com>
2016-10-16 17:08:46 +0200

    Added assetic bundle

---
+++ src/AppBundle/Controller/AdminController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Electra;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+
+class AdminController extends Controller
+{
+    /**
+     * @Route("/user", name="admin")
+     */
+    public function indexAction()
+    {
+        $user = $this->getUser();
+        return $this->render('admin/content.html.twig', [
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * @Route("/user/electra", name="electra")
+     */
+    public function createElectraAction(Request $request)
+    {
+        $electra = new Electra();
+
+        $form = $this->createFormBuilder($electra)
+            ->add('title', TextType::class)
+            ->add('shortText', TextType::class)
+            ->add('text', TextType::class)
+            ->add('link', TextType::class)
+            ->add('active', CheckboxType::class)
+            ->add('imageId', TextType::class)
+            ->add('Submit', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $data = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($data);
+            $em->flush();
+        }
+
+        return $this->render('admin/electra.html.twig',[
+            'form' => $form->createView()
+        ]);
+    }
+
+}
---
+++ src/AppBundle/Entity/Electra.php
@@ -0,0 +1,221 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Electra
+ *
+ * @ORM\Table(name="electra")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ElectraRepository")
+ */
+class Electra
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="shortText", type="string", length=255)
+     */
+    private $shortText;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="text", type="text")
+     */
+    private $text;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="link", type="string", length=255)
+     */
+    private $link;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="imageId", type="text")
+     */
+    private $imageId;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="active", type="boolean")
+     */
+    private $active;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Electra
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set shortText
+     *
+     * @param string $shortText
+     *
+     * @return Electra
+     */
+    public function setShortText($shortText)
+    {
+        $this->shortText = $shortText;
+
+        return $this;
+    }
+
+    /**
+     * Get shortText
+     *
+     * @return string
+     */
+    public function getShortText()
+    {
+        return $this->shortText;
+    }
+
+    /**
+     * Set text
+     *
+     * @param string $text
+     *
+     * @return Electra
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * Get text
+     *
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * Set link
+     *
+     * @param string $link
+     *
+     * @return Electra
+     */
+    public function setLink($link)
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+
+    /**
+     * Get link
+     *
+     * @return string
+     */
+    public function getLink()
+    {
+        return $this->link;
+    }
+
+    /**
+     * Set imageId
+     *
+     * @param string $imageId
+     *
+     * @return Electra
+     */
+    public function setImageId($imageId)
+    {
+        $this->imageId = $imageId;
+
+        return $this;
+    }
+
+    /**
+     * Get imageId
+     *
+     * @return string
+     */
+    public function getImageId()
+    {
+        return $this->imageId;
+    }
+
+    /**
+     * Set active
+     *
+     * @param boolean $active
+     *
+     * @return Electra
+     */
+    public function setActive($active)
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+
+    /**
+     * Get active
+     *
+     * @return bool
+     */
+    public function getActive()
+    {
+        return $this->active;
+    }
+}
+
