0a13ceaa18ab7b6d0f335407144026637e303560
Yaron Lambers <y.lambers@outlook.com>
2016-10-16 14:55:30 +0200

    Form add content

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,15 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+        $electra = $this->getDoctrine()
+            ->getRepository('AppBundle:Electra')
+            ->findBy([
+                'active' => 1
+            ]);
+
+
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+            'electra' => $electra
         ]);
     }
 }
