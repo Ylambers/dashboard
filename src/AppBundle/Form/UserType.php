a3972241a5857139920a7b2e71383b483b2563de
Yaron Lambers <yaronlambers@yarons-MacBook-Pro.local>
2016-11-14 15:12:30 +0100

    added enabled option to form

--- src/AppBundle/Form/UserType.php
+++ src/AppBundle/Form/UserType.php
@@ -8,8 +8,10 @@
 
 namespace AppBundle\Form;
 
+use AppBundle\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -21,37 +23,14 @@ class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $permissions = array(
-            'ROLE_USER'        => 'First role',
-            'ROLE_CONSULT'     => 'Second role',
-            'ROLE_SUPER_ADMIN' => 'Third role'
-        );
-
         $builder
             ->add('username', TextType::class,[
             ])
             ->add('email', TextType::class,[])
-            ->add('roles', 'choice', array(
-                'choices' => $this->getExistingRoles(),
-                'data' => $group->getRoles(),
-                'label' => 'Roles',
-                'expanded' => true,
-                'multiple' => true,
-                'mapped' => true,
-            ))
+            ->add('enabled', CheckboxType::class, [
+                'required' => false
+            ])
             ->add('Submit', SubmitType::class)
             ->getForm();
-    }
-
-    public function getExistingRoles()
-    {
-//        $roleHierarchy = $this->container->getParameter('security.role_hierarchy.roles');
-        $roleHierarchy = $this->
-        $roles = array_keys($roleHierarchy);
-
-        foreach ($roles as $role) {
-            $theRoles[$role] = $role;
-        }
-        return $theRoles;
     }
 }
